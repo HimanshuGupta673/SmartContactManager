b3b590e0bec489b1a81076ce746d5663d013e42d
HimanshuGupta673 <himanshuu673@gmail.com>
2024-02-20 18:38:40 +0530

    show contacts with pagination

--- src/main/java/com/learner/smartContactManager/controller/UserController.java
+++ src/main/java/com/learner/smartContactManager/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.learner.smartContactManager.controller;
 
+import com.learner.smartContactManager.dao.ContactRepository;
 import com.learner.smartContactManager.dao.UserRepository;
 import com.learner.smartContactManager.entities.Contact;
 import com.learner.smartContactManager.entities.User;
@@ -7,12 +8,12 @@ import com.learner.smartContactManager.helper.Message;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -21,13 +22,15 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
-
+    @Autowired
+    private ContactRepository contactRepository;
     @ModelAttribute
     public void addCommonData(Model model, Principal principal){
         String userName = principal.getName();
@@ -77,9 +80,18 @@ public class UserController {
         return "normal/addContact";
     }
 
-    @RequestMapping("/showContacts")
-    public String showContacts(){
-        return "showContacts";
+    @RequestMapping("/showContacts/{page}")
+    public String showContacts(@PathVariable("page") Integer page, Model model, Principal principal){
+        model.addAttribute("title","Show Contacts");
+        String userName = principal.getName();
+        User user = userRepository.getUserByUserName(userName);
+
+        Pageable pageable = PageRequest.of(page,5);
+        Page<Contact> contacts = contactRepository.findContactByUser(user.getId(),pageable);
+        model.addAttribute("contacts",contacts);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("totalPages",contacts.getTotalPages());
+        return "normal/showContacts";
     }
 
 }
