6ab41b166ce972351974ce38c4ae4475684bf2f4
Himanshu Gupta <112652867+HimanshuGupta673@users.noreply.github.com>
2024-07-23 00:33:04 +0530

    Deployment configuration handled

--- src/main/java/com/learner/smartContactManager/SmartContactManagerApplication.java
+++ src/main/java/com/learner/smartContactManager/SmartContactManagerApplication.java
@@ -2,6 +2,8 @@ package com.learner.smartContactManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 
 @SpringBootApplication
 public class SmartContactManagerApplication {
